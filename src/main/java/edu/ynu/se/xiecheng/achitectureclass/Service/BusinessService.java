8eaf549b82cdd78657a81ea19c9cc3ed582ead72
谢庚言 <835456664@qq.com>
2024-01-06 22:41:46 +0800

    商家服务层增加查看所有订单，查看店铺订单，确认订单，取消订单功能

--- src/main/java/edu/ynu/se/xiecheng/achitectureclass/Service/BusinessService.java
+++ src/main/java/edu/ynu/se/xiecheng/achitectureclass/Service/BusinessService.java
@@ -3,20 +3,22 @@ package edu.ynu.se.xiecheng.achitectureclass.Service;
 import edu.ynu.se.xiecheng.achitectureclass.common.service.LogicService;
 import edu.ynu.se.xiecheng.achitectureclass.dao.BusinessDao;
 import edu.ynu.se.xiecheng.achitectureclass.dao.ItemDao;
+import edu.ynu.se.xiecheng.achitectureclass.dao.OrderDao;
 import edu.ynu.se.xiecheng.achitectureclass.dao.ShopDao;
-import edu.ynu.se.xiecheng.achitectureclass.entity.Business;
-import edu.ynu.se.xiecheng.achitectureclass.entity.Item;
-import edu.ynu.se.xiecheng.achitectureclass.entity.Shop;
-import edu.ynu.se.xiecheng.achitectureclass.entity.ShopItem;
+import edu.ynu.se.xiecheng.achitectureclass.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Resource;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BusinessService extends LogicService<BusinessDao, Business, Long> {
     @Resource
     private ItemDao itemDao;
     @Resource
     private ShopDao shopDao;
+    @Resource
+    private OrderDao orderDao;
     public BusinessService(@Autowired BusinessDao businessDao){
         super(businessDao);
     }
@@ -43,5 +45,34 @@ public class BusinessService extends LogicService<BusinessDao, Business, Long> {
         ShopItem shopItem = business.addShopItem(item,shop);
         getDAO().save(business);
         return shopItem;
+    }
+
+    public Set<Order> findAllOrders(Long businessId){
+        Business business = getDAO().getReferenceById(businessId);
+        return business.findAllOrders();
+    }
+
+    public Set<Order> findShopOrders(Long businessId,Long shopId){
+        Business business = getDAO().getReferenceById(businessId);
+        Shop shop = shopDao.getReferenceById(shopId);
+        return business.findShopOrders(shop);
+    }
+
+    public Order confirm(Long businessId,Long shopId,Long orderId){
+        Business business = getDAO().getReferenceById(businessId);
+        Shop shop = shopDao.getReferenceById(shopId);
+        Order order = orderDao.getReferenceById(orderId);
+        order = business.confirm(shop,order);
+        getDAO().save(business);
+        return order;
+    }
+
+    public Order refund(Long businessId,Long shopId,Long orderId){
+        Business business = getDAO().getReferenceById(businessId);
+        Shop shop = shopDao.getReferenceById(shopId);
+        Order order = orderDao.getReferenceById(orderId);
+        order = business.refund(shop,order);
+        getDAO().save(business);
+        return order;
     }
 }
