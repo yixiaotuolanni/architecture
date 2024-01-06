d8d97318ae5b2a094d7283535bfb6bd4a6604da8
谢庚言 <835456664@qq.com>
2024-01-06 16:48:39 +0800

    商家模块dao层创建

---
+++ src/main/java/edu/ynu/se/xiecheng/achitectureclass/dao/BusinessDao.java
@@ -0,0 +1,7 @@
+package edu.ynu.se.xiecheng.achitectureclass.dao;
+
+import edu.ynu.se.xiecheng.achitectureclass.common.dao.LogicDAO;
+import edu.ynu.se.xiecheng.achitectureclass.entity.Business;
+
+public interface BusinessDao extends LogicDAO<Business,Long> {
+}
