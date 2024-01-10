5309299912ea703ce261e474afd1be8f621266aa
谢庚言 <835456664@qq.com>
2024-01-10 13:31:53 +0800

    controller完善登录功能

--- src/main/java/edu/ynu/se/xiecheng/achitectureclass/controller/BusinessController.java
+++ src/main/java/edu/ynu/se/xiecheng/achitectureclass/controller/BusinessController.java
@@ -2,77 +2,103 @@ package edu.ynu.se.xiecheng.achitectureclass.controller;
 
 import edu.ynu.se.xiecheng.achitectureclass.Service.BusinessService;
 import edu.ynu.se.xiecheng.achitectureclass.common.controller.LogicController;
+import edu.ynu.se.xiecheng.achitectureclass.common.utils.JwtTokenUtil;
 import edu.ynu.se.xiecheng.achitectureclass.dao.BusinessDao;
+import edu.ynu.se.xiecheng.achitectureclass.dto.BusinessDTO;
+import edu.ynu.se.xiecheng.achitectureclass.dto.RequestData;
 import edu.ynu.se.xiecheng.achitectureclass.entity.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 @Api(tags = "商家实体的控制器")
 @RestController
 @RequestMapping("/business")
 public class BusinessController extends LogicController<BusinessService, BusinessDao, Business, Long> {
-
     @Resource
     private ModelMapper modelMapper;
+    @Resource
+    private JwtTokenUtil jwtTokenUtil;
     public BusinessController(@Autowired BusinessService businessService){
         super(businessService);
     }
+    @ApiOperation("商家登录")
+    @PostMapping("/login")
+    public String login(@ApiParam("电话号码") @RequestParam String phoneNumber,
+                        @ApiParam("密码") @RequestParam String password){
+        return jwtTokenUtil.generateToken(getService().login(phoneNumber, password).getId());
+    }
 
+    @ApiOperation("商家通过token登录")
+    @PostMapping("/findBusiness")
+    public Business findBusiness(@ApiParam("token") String token){
+        return getService().findBusiness(jwtTokenUtil.getUseIdFromToken(token));
+    }
     @ApiOperation("商家创建店铺")
-    @GetMapping("/addShop")
-    public Shop addShop(@ApiParam("商家号")Long businessId,
+    @PostMapping("/addShop")
+    public Shop addShop(@ApiParam("token")String token,
                         @ApiParam("店铺名称") String shopName,
+                        @ApiParam("店铺电话") String phoneNumber,
+                        @ApiParam("店铺地址") String address,
                         @ApiParam("店铺描述") String shopDescription){
-        return getService().addShop(businessId,shopName,shopDescription);
+        return getService().addShop(jwtTokenUtil.getUseIdFromToken(token),shopName,phoneNumber,address,shopDescription);
     }
 
     @ApiOperation("商家创建商品")
-    @GetMapping("/addItem")
-    public Item addItem(@ApiParam("商家号")Long businessId,
+    @PostMapping("/addItem")
+    public Item addItem(@ApiParam("token")String token,
                         @ApiParam("商品名称")String itemName,
                         @ApiParam("商品价格")Double itemPrice,
                         @ApiParam("商品描述")String itemDescription){
-        return getService().addItem(businessId, itemName, itemPrice, itemDescription);
+        return getService().addItem(jwtTokenUtil.getUseIdFromToken(token), itemName, itemPrice, itemDescription);
     }
 
     @ApiOperation("商家上架商品")
-    @GetMapping("/addShopItem")
-    public ShopItem addShopItem(@ApiParam("商家号")Long businessId,
+    @PostMapping("/addShopItem")
+    public ShopItem addShopItem(@ApiParam("token")String token,
                                 @ApiParam("商品号")Long itemId,
                                 @ApiParam("店铺号")Long shopId){
-        return getService().addShopItem(businessId, itemId, shopId);
+        return getService().addShopItem(jwtTokenUtil.getUseIdFromToken(token), itemId, shopId);
+    }
+    @ApiOperation("商家上架一系列商品")
+    @PostMapping("/addShopItems")
+    public Set<ShopItem> addShopItems(@RequestBody RequestData requestData){
+        String token = requestData.getToken();
+        List<Long> itemsId = requestData.getItemsId();
+        Long shopId = requestData.getShopId();
+        return getService().addShopItems(jwtTokenUtil.getUseIdFromToken(token), itemsId, shopId);
     }
     @ApiOperation("商家查找所有订单")
-    @GetMapping("/findAllOrders")
-    public Set<Order> findAllOrders(@ApiParam("商家号")Long businessId){
-        return getService().findAllOrders(businessId);
+    @PostMapping("/findAllOrders")
+    public Set<Order> findAllOrders(@ApiParam("token")String token){
+        return getService().findAllOrders(jwtTokenUtil.getUseIdFromToken(token));
     }
     @ApiOperation("商家查找店铺订单")
-    @GetMapping("/findShopOrders")
-    public Set<Order> findShopOrders(@ApiParam("商家号")Long businessId,
+    @PostMapping("/findShopOrders")
+    public Set<Order> findShopOrders(@ApiParam("token")String token,
                                      @ApiParam("店铺号")Long shopId){
-        return getService().findShopOrders(businessId, shopId);
+        return getService().findShopOrders(jwtTokenUtil.getUseIdFromToken(token), shopId);
     }
     @ApiOperation("商家确认订单")
-    @GetMapping("/confirm")
-    public Order confirm(@ApiParam("商家号")Long businessId,
+    @PostMapping("/confirm")
+    public Order confirm(@ApiParam("token")String token,
                          @ApiParam("店铺号")Long shopId,
                          @ApiParam("订单号") Long orderId){
-        return getService().confirm(businessId,shopId,orderId);
+        return getService().confirm(jwtTokenUtil.getUseIdFromToken(token),shopId,orderId);
     }
     @ApiOperation("商家取消订单")
-    @GetMapping("/refund")
-    public Order refund(@ApiParam("商家号")Long businessId,
+    @PostMapping("/refund")
+    public Order refund(@ApiParam("token")String token,
                         @ApiParam("店铺号")Long shopId,
                         @ApiParam("订单号") Long orderId){
-        return getService().refund(businessId, shopId, orderId);
+        return getService().refund(jwtTokenUtil.getUseIdFromToken(token), shopId, orderId);
     }
 }
--- src/main/java/edu/ynu/se/xiecheng/achitectureclass/controller/CustomerController.java
+++ src/main/java/edu/ynu/se/xiecheng/achitectureclass/controller/CustomerController.java
@@ -2,6 +2,7 @@ package edu.ynu.se.xiecheng.achitectureclass.controller;
 
 import edu.ynu.se.xiecheng.achitectureclass.Service.CustomerService;
 import edu.ynu.se.xiecheng.achitectureclass.common.controller.LogicController;
+import edu.ynu.se.xiecheng.achitectureclass.common.utils.JwtTokenUtil;
 import edu.ynu.se.xiecheng.achitectureclass.dao.CustomerDao;
 import edu.ynu.se.xiecheng.achitectureclass.entity.*;
 import io.swagger.annotations.Api;
@@ -9,31 +10,47 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Set;
+
 @Api(tags = "顾客实体的控制器")
 @RestController
 @RequestMapping("/customer")
 public class CustomerController extends LogicController<CustomerService, CustomerDao, Customer,Long> {
     @Resource
     private ModelMapper modelMapper;
+    @Resource
+    private JwtTokenUtil jwtTokenUtil;
     public CustomerController(CustomerService customerService) {
         super(customerService);
     }
+    @ApiOperation("顾客登录")
+    @PostMapping("/login")
+    public String login(@ApiParam("电话号码")String phoneNumber,
+                        @ApiParam("密码") String password){
+        return jwtTokenUtil.generateToken(getService().login(phoneNumber, password).getId());
+    }
+
+    @ApiOperation("顾客通过token登录")
+    @PostMapping("/findCustomer")
+    public Customer findCustomer(@ApiParam("token") String token){
+        return getService().findCustomer(jwtTokenUtil.getUseIdFromToken(token));
+    }
+
     @ApiOperation("顾客挑选商品")
-    @GetMapping("/addLineItem")
-    public LineItem addLineItem(@ApiParam("顾客号") Long customerId,
+    @PostMapping("/addLineItem")
+    public LineItem addLineItem(@ApiParam("token")String token,
                                 @ApiParam("数量") Double amount,
                                 @ApiParam("店铺商品号") Long shopItemId){
-        return getService().addLineItem(customerId, amount, shopItemId);
+        return getService().addLineItem(jwtTokenUtil.getUseIdFromToken(token), amount, shopItemId);
     }
     @ApiOperation("顾客付款")
-    @GetMapping("/pay")
-    public Order pay(@ApiParam("顾客号") Long customerId,
+    @PostMapping("/pay")
+    public Order pay(@ApiParam("token")String token,
                      @ApiParam("订单号") Long orderId){
-        return getService().pay(customerId, orderId);
+        return getService().pay(jwtTokenUtil.getUseIdFromToken(token), orderId);
     }
 }
