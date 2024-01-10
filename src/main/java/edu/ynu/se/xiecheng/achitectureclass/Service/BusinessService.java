06e9ae557a98bd4cd263219f8fb852c76a9602ff
谢庚言 <835456664@qq.com>
2024-01-10 13:29:40 +0800

    增加登录和通过id找到用户的功能，并进行完善

--- src/main/java/edu/ynu/se/xiecheng/achitectureclass/Service/BusinessService.java
+++ src/main/java/edu/ynu/se/xiecheng/achitectureclass/Service/BusinessService.java
@@ -8,10 +8,14 @@ import edu.ynu.se.xiecheng.achitectureclass.dao.ShopDao;
 import edu.ynu.se.xiecheng.achitectureclass.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 @Service
 public class BusinessService extends LogicService<BusinessDao, Business, Long> {
     @Resource
@@ -20,59 +24,84 @@ public class BusinessService extends LogicService<BusinessDao, Business, Long> {
     private ShopDao shopDao;
     @Resource
     private OrderDao orderDao;
-    public BusinessService(@Autowired BusinessDao businessDao){
+
+    public BusinessService(@Autowired BusinessDao businessDao) {
         super(businessDao);
     }
 
-    public Shop addShop(Long businessId,String shopName, String shopDescription){
+    public Business login(String phoneNumber, String password) {
+        return getDAO().findBusinessByPhoneNumberAndPassword(phoneNumber, DigestUtils.md5DigestAsHex(password.getBytes()));
+    }
+
+    public Business findBusiness(Long businessId) {
+        return getDAO().getReferenceById(businessId);
+    }
+
+    @Transactional
+    public Shop addShop(Long businessId, String shopName, String phoneNumber, String address, String shopDescription) {
         Business business = getDAO().getReferenceById(businessId);
-        Shop shop = business.addShop(shopName, shopDescription);
+        Shop shop = business.addShop(shopName, phoneNumber, address, shopDescription);
         getDAO().save(business);
         return shop;
     }
 
-    public Item addItem(Long businessId,String itemName, Double itemPrice, String itemDescription){
+    @Transactional
+    public Item addItem(Long businessId, String itemName, Double itemPrice, String itemDescription) {
         Business business = getDAO().getReferenceById(businessId);
         Item item = business.addItem(itemName, itemPrice, itemDescription);
         getDAO().save(business);
         return item;
     }
 
-    public ShopItem addShopItem(Long businessId,Long itemId, Long shopId){
+    @Transactional
+    public ShopItem addShopItem(Long businessId, Long itemId, Long shopId) {
         Business business = getDAO().getReferenceById(businessId);
         Item item = itemDao.getReferenceById(itemId);
         Shop shop = shopDao.getReferenceById(shopId);
 
-        ShopItem shopItem = business.addShopItem(item,shop);
+        ShopItem shopItem = business.addShopItem(item, shop);
         getDAO().save(business);
         return shopItem;
     }
 
-    public Set<Order> findAllOrders(Long businessId){
+    @Transactional
+    public Set<ShopItem> addShopItems(Long businessId, List<Long> itemId, Long shopId) {
+        Business business = getDAO().getReferenceById(businessId);
+        List<Item> items = itemDao.findAllById(itemId);
+        Shop shop = shopDao.getReferenceById(shopId);
+
+        Set<ShopItem> shopItems = business.addShopItems(items, shop);
+        getDAO().save(business);
+        return shopItems;
+    }
+
+    public Set<Order> findAllOrders(Long businessId) {
         Business business = getDAO().getReferenceById(businessId);
         return business.findAllOrders();
     }
 
-    public Set<Order> findShopOrders(Long businessId,Long shopId){
+    public Set<Order> findShopOrders(Long businessId, Long shopId) {
         Business business = getDAO().getReferenceById(businessId);
         Shop shop = shopDao.getReferenceById(shopId);
         return business.findShopOrders(shop);
     }
 
-    public Order confirm(Long businessId,Long shopId,Long orderId){
+    @Transactional
+    public Order confirm(Long businessId, Long shopId, Long orderId) {
         Business business = getDAO().getReferenceById(businessId);
         Shop shop = shopDao.getReferenceById(shopId);
         Order order = orderDao.getReferenceById(orderId);
-        order = business.confirm(shop,order);
+        order = business.confirm(shop, order);
         getDAO().save(business);
         return order;
     }
 
-    public Order refund(Long businessId,Long shopId,Long orderId){
+    @Transactional
+    public Order refund(Long businessId, Long shopId, Long orderId) {
         Business business = getDAO().getReferenceById(businessId);
         Shop shop = shopDao.getReferenceById(shopId);
         Order order = orderDao.getReferenceById(orderId);
-        order = business.refund(shop,order);
+        order = business.refund(shop, order);
         getDAO().save(business);
         return order;
     }
--- src/main/java/edu/ynu/se/xiecheng/achitectureclass/Service/CustomerService.java
+++ src/main/java/edu/ynu/se/xiecheng/achitectureclass/Service/CustomerService.java
@@ -6,6 +6,8 @@ import edu.ynu.se.xiecheng.achitectureclass.dao.OrderDao;
 import edu.ynu.se.xiecheng.achitectureclass.dao.ShopItemDao;
 import edu.ynu.se.xiecheng.achitectureclass.entity.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
 @Service
@@ -18,6 +20,15 @@ public class CustomerService extends LogicService<CustomerDao, Customer,Long> {
         super(customerDao);
     }
 
+    public Customer login(String phoneNumber, String password){
+        return getDAO().findCustomerByPhoneNumberAndPassword(phoneNumber, DigestUtils.md5DigestAsHex(password.getBytes()));
+    }
+
+    public Customer findCustomer(Long customerId){
+        return getDAO().getReferenceById(customerId);
+    }
+
+    @Transactional
     public LineItem addLineItem(Long customerId,Double amount, Long shopItemId){
         Customer customer = getDAO().getReferenceById(customerId);
         ShopItem shopItem = shopItemDao.getReferenceById(shopItemId);
@@ -26,6 +37,7 @@ public class CustomerService extends LogicService<CustomerDao, Customer,Long> {
         return lineItem;
     }
 
+    @Transactional
     public Order pay(Long customerId,Long orderId){
         Customer customer = getDAO().getReferenceById(customerId);
         Order order = orderDao.getReferenceById(orderId);
